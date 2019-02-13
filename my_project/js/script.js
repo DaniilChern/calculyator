90ceb7153d75580695370039573ca247dec4070b
DaniilChern <46822766+DaniilChern@users.noreply.github.com>
2019-02-13 09:57:43 +0700

    Add files via upload

---
+++ my_project/js/script.js
@@ -0,0 +1,36 @@
+function pow2(){
+   let num1;
+   num1 = document.getElementById("inp1").value;
+   num1 = +num1;
+   let num2;
+   num2 = document.getElementById("inp2").value;
+   num2 = +num2;
+   alert( num1+num2);
+}
+function pow3(){
+   let num1;
+   num1 = document.getElementById("inp1").value;
+   num1 = +num1;
+   let num2;
+   num2 = document.getElementById("inp2").value;
+   num2 = +num2;
+   alert(num1-num2);
+}
+function pow4(){
+   let num1;
+   num1 = document.getElementById("inp1").value;
+   num1 = +num1;
+   let num2;
+   num2 = document.getElementById("inp2").value;
+   num2 = +num2;
+   alert(num1*num2);
+}
+function pow5(){
+   let num1;
+   num1 = document.getElementById("inp1").value;
+   num1 = +num1;
+   let num2;
+   num2 = document.getElementById("inp2").value;
+   num2 = +num2;
+   alert(num1/num2);
+}
