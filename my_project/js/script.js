b359683b451e7159043f7f504d6cf066c8cdf55e
DaniilChern <46822766+DaniilChern@users.noreply.github.com>
2019-02-13 10:56:05 +0700

    Update script.js

--- my_project/js/script.js
+++ my_project/js/script.js
@@ -1,28 +1,36 @@
 function pow2(){
    let num1 = document.getElementById("inp1").value;
-   num1 = +num1;
+   numNumber1 = +num1;
    let num2 = document.getElementById("inp2").value;
-   num2 = +num2;
-   alert( num1+num2);
+   numNumber2 = +num2;
+   numNumber3 = numNumber1+numNumber2;
+   let ind1=[num1 ," + ", num2," = ",numNumber3];
+   alert(ind1.join(" "));
 }
 function pow3(){
    let num1 = document.getElementById("inp1").value;
-   num1 = +num1;
+   numNumber1 = +num1;
    let num2 = document.getElementById("inp2").value;
-   num2 = +num2;
-   alert(num1-num2);
+   numNumber2 = +num2;
+   numNumber3 = numNumber1-numNumber2;
+   let ind1=[num1 ," - ", num2," = ",numNumber3];
+   alert(ind1.join(" "));
 }
 function pow4(){
    let num1 = document.getElementById("inp1").value;
-   num1 = +num1;
+   numNumber1 = +num1;
    let num2 = document.getElementById("inp2").value;
-   num2 = +num2;
-   alert(num1*num2);
+   numNumber2 = +num2;
+   numNumber3 = numNumber1*numNumber2;
+   let ind1=[num1 ," * ", num2," = ",numNumber3];
+   alert(ind1.join(" "));
 }
 function pow5(){
    let num1 = document.getElementById("inp1").value;
-   num1 = +num1;
+   numNumber1 = +num1;
    let num2 = document.getElementById("inp2").value;
-   num2 = +num2;
-   alert(num1/num2);
+   numNumber2 = +num2;
+   numNumber3 = numNumber1/numNumber2;
+   let ind1=[num1 ," / ", num2," = ",numNumber3];
+   alert(ind1.join(" "));
 }
